15e8db80927bbeddd8a366ae1a761687642e1053
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2020-04-23 10:52:38 -0300

    First Commit

---
+++ MentalCampfire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MentalCampfire
+//
+//  Created by Rodrigo Silva Ribeiro on 23/04/20.
+//  Copyright © 2020 Rodrigo Silva Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
