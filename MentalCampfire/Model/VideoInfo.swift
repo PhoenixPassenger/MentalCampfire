48a5510985e131768e8e9fb244a8da9917659699
Rodrigo Silva Ribeiro <Rodrigo.ato@hotmail.com>
2020-05-06 11:25:16 -0300

    add segues

---
+++ MentalCampfire/Model/VideoInfo.swift
@@ -0,0 +1,14 @@
+//
+//  AudioInfo.swift
+//  MentalCampfire
+//
+//  Created by Rodrigo Silva Ribeiro on 06/05/20.
+//  Copyright © 2020 Rodrigo Silva Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    var name:String
+    var imageName:String
+}
